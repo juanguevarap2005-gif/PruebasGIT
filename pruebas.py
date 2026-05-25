4370a4bfe5fb9e4ae75b4c1cbef11579df0719bf
JuanGuevara <juanguevarap2005@gmail.com>
2026-05-25 01:57:33 -0500

    Ciclo

--- pruebas.py
+++ pruebas.py
@@ -1,2 +1,19 @@
 print("inicio proyecto")
+#Definicion de varibales 
+nombre=input("Juan Guevara")
+Edad = int(input("21"))
 
+print("Hola,nombre")
+print("Tu edad es:",edad)
+
+#Condicion 
+if edad >= "18":
+    print ("eres mayor de edad")
+else:
+    print("eres menor de edad")
+    
+    #Ciclo
+    for i in range(3):
+        print("Bienvenido al sistema",nombre)
+        
+        
